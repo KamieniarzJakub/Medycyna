b764ae0dd43b2ced3376f2ce4fc7ddc928af6f6f
KamieniarzJakub <jakub.kamieniarz@student.put.poznan.pl>
2025-03-19 12:32:35 +0100

    Sinogram

--- tomograf.py
+++ tomograf.py
@@ -23,26 +23,42 @@ def bresenham_line(x0, y0, x1, y1):
     
     return points
 
-img = cv2.imread("img/Kolo.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("img/Kropka.jpg", cv2.IMREAD_GRAYSCALE)
 h, w = img.shape
 img = cv2.copyMakeBorder(img, max((w-h)//2, 0), max((w-h)//2, 0), max((h-w)//2, 0), max((h-w)//2, 0), cv2.BORDER_CONSTANT, value=[0])
 h, w = img.shape
 print(img.shape)
+angle_step = 2
+n = 180 #Liczba emiterów/detetektorów
+emiters_angles = 30  #Kąt rozposzenia emiterów
 
-angle_step = 0
-n = 10
-emiters_angles = 45
-emiters_angles = np.linspace(angle_step, angle_step + emiters_angles, n)
+angles = np.arange(0, 180, angle_step)
+sinogram = np.zeros((len(angles), n))
 
 r = w//2
-for angle in emiters_angles:
-    angle_rad = np.deg2rad(angle)
-    x_e = r-int(r * np.sin(angle_rad))
-    y_e = r-int(r * np.cos(angle_rad))
-    img[y_e][x_e] = 255
-    img[h-y_e-1][w-x_e-1] = 255
-    for x,y in bresenham_line(x_e, y_e, h-x_e-1, w-y_e-1):
-        img[y][x] = 100
+max_value = 0
+for a, angle in enumerate(angles):
+    detectors_angles = np.linspace(angle-emiters_angles//2, angle+emiters_angles//2, n)
+    for i, emiter_angle in enumerate(detectors_angles):
+        emiter_angle_rad = np.deg2rad(emiter_angle)
+        detector_angle_rad = np.deg2rad(detectors_angles[n-1-i])
+        x_e = min(r-int(r * np.sin(emiter_angle_rad)), w-1)
+        x_d = min(r-int(r * np.sin(np.pi+detector_angle_rad)), w-1)
+        y_e = min(r-int(r * np.cos(emiter_angle_rad)), h-1)
+        y_d = min(r-int(r * np.cos(np.pi+detector_angle_rad)), h-1)
+        #img[y_e][x_e] = 255
+        #img[y_d][x_d] = 255
+
+        for x,y in bresenham_line(x_e, y_e, x_d, y_d):
+            sinogram[a][i]+=img[y][x]
+            #img[y][x] = 100
+        if sinogram[a][i]>max_value:
+            max_value = sinogram[a][i]
+sinogram/=max_value
+sinogram*=255
+#print(sinogram[0][90], max_value)
+
+cv2.imshow("sinogram", sinogram)
 
 cv2.imshow("tomograf", img)
 cv2.waitKey()
