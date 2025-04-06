737e051eac15e1d14338fa2c19ddbe3ccaf4adee
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-04-06 16:36:49 +0200

    bugfixes

--- s.py
+++ s.py
@@ -10,31 +10,31 @@ import cv2
 # Streamlit app
 st.title("Tomograf")
 
-st.write("flkjaslkdj")
-
 # Slider to adjust the parameter for image generation
-slider_value = st.slider("Adjust the slider:", 0, 100, 50)
-
-# Generate the image based on slider value
-# image = generate_image(slider_value)
-
+krok_ukladu = st.slider("Krok ∆α układu emiter/detektor:", 1, 10, 1)
+liczba_detektorów = st.slider(
+    "Dla jednego układu emiter/detektor liczbę detektorów (n):", 1, 500, 180
+)
+rozwartosc = st.slider("Rozwartość/rozpiętość układu emiter/detektor (l)", 0, 180, 90)
 
 # Display the generated image using matplotlib
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-# contents = cv2.imread("img/Kropka.jpg", cv2.IMREAD_GRAYSCALE)
-# contents = Image.open()
-# pimg = Image.open("img/Kropka.jpg")
-# pimg =
-# image = cv2.cvt(, cv2.IMREAD_GRAYSCALE)
+(fig1, ax1) = plt.subplots()
 image = cv2.imread("img/Kropka.jpg", cv2.IMREAD_GRAYSCALE)
 ax1.imshow(image, cmap="gray")
 ax1.axis("off")
+st.pyplot(fig1)
+
+fig2, ax2 = plt.subplots()
 sinogram = tomograf.radon(image, 2, 1000)
 # sinogram = tomograf.radom_full(image, image.shape)
 ax2.imshow(sinogram, cmap="gray")
 ax2.axis("off")
+st.pyplot(fig2)
+
+
+fig3, ax3 = plt.subplots()
 reconstructed = tomograf.inverse_radon(sinogram, image.shape, 2, 1000)
 # reconstructed = tomograf.radom_full(image, image.shape, inverse=True)
 ax3.imshow(reconstructed, cmap="gray")
 ax3.axis("off")
-st.pyplot(fig)
+st.pyplot(fig3)
--- tomograf.py
+++ tomograf.py
@@ -1,6 +1,5 @@
 import numpy as np
 from numba import jit
-import cv2
 
 
 @jit
@@ -33,23 +32,26 @@ def radon_init(angle_step, num_emiters, full_scan_range=360):
     return scans_angle_labels, out
 
 
-# @jit
+@jit
+def radon_clean(out):
+    kernel = convolve_kernel(n=21)
+    for a in out:
+        out[a] = np.convolve(out[a, :], kernel, mode="same")
+    out /= out.max()
+
+
+@jit
 def radon(img, angle_step, num_emiters, full_scan_range=360):
     w, h = img.shape
     # scans_angle_labels, out = radon_init(angle_step, num_emiters, full_scan_range)
     out = np.zeros((full_scan_range // angle_step, num_emiters))
-    # img2 = np.zeros(img.shape)
     for a, i, y, x in radon_step(angle_step, num_emiters, w, h, 90, out):
-        # img2[y][x] = 255
-        # cv2.imshow("source 2", img2)
-        # cv2.waitKey(1)
         out[a][i] += img[y][x]
 
-    out /= out.max()
     return out
 
 
-# @jit
+@jit
 def inverse_radon(sinogram, size, angle_step, num_emiters):
     h, w = size
     out = np.zeros(size)
@@ -61,7 +63,9 @@ def inverse_radon(sinogram, size, angle_step, num_emiters):
     return out
 
 
-def radon_single_beam(angle, detectors_angle, shift, i, r):
+@jit
+def radon_single_beam(angle, detectors_angle, shift, i, w, h):
+    r = w / 2
     emiter = angle - detectors_angle / 2 + i * shift
     emiter_angle_rad = np.deg2rad(emiter)
     x_e = min(int(r - r * np.cos(emiter_angle_rad)), w - 1)
@@ -77,7 +81,8 @@ def radon_single_beam(angle, detectors_angle, shift, i, r):
     return line
 
 
-def radon_emiter_detector(angle, detectors_angle, shift, i, r):
+def radon_emiter_detector(angle, detectors_angle, shift, i, w, h):
+    r = w / 2
     emiter = angle - detectors_angle / 2 + i * shift
     emiter_angle_rad = np.deg2rad(emiter)
     x_e = min(int(r - r * np.cos(emiter_angle_rad)), w - 1)
@@ -90,62 +95,13 @@ def radon_emiter_detector(angle, detectors_angle, shift, i, r):
     return (x_e, y_e, x_d, y_d)
 
 
-# @jit
+@jit
 def radon_step(angle_step, n, w, h, detectors_angle, out):
-    r = w / 2
     shift = detectors_angle / (n - 1)
     for a, angle in enumerate(range(0, 360, angle_step)):
         for i in range(n):
-            for b in radon_single_beam(angle, detectors_angle, shift, i, r):
+            for b in radon_single_beam(angle, detectors_angle, shift, i, w, h):
                 yield (a, *b)
-        # cv2.imshow("sinogram", out)
-        # cv2.waitKey(1)
-
-
-def radom_full(
-    img,
-    size=None,
-    angle_step=2,
-    n=180,
-    emiters_angles=180,
-    inverse=False,
-):
-    # n - Liczba emiterów/detetektorów
-    # emiters_angles = 180  # Kąt rozposzenia emiterów
-    if size is None:
-        size = img.shape
-    h, w = size
-    r = w // 2
-    angles = np.arange(0, 180, angle_step)
-    if inverse:
-        out = np.zeros(size)
-    else:
-        out = np.zeros((len(angles), n))
-
-    for a, angle in enumerate(angles):
-        detectors_angles = np.linspace(
-            angle - emiters_angles // 2, angle + emiters_angles // 2, n
-        )
-        for i, emiter_angle in enumerate(detectors_angles):
-            emiter_angle_rad = np.deg2rad(emiter_angle)
-            detector_angle_rad = np.deg2rad(detectors_angles[n - 1 - i])
-            x_e = min(r - int(r * np.cos(emiter_angle_rad)), w - 1)
-            x_d = min(r - int(r * np.cos(np.pi + detector_angle_rad)), w - 1)
-            y_e = min(r - int(r * np.sin(emiter_angle_rad)), h - 1)
-            y_d = min(r - int(r * np.sin(np.pi + detector_angle_rad)), h - 1)
-            # img[y_e][x_e] = 255
-            # img[y_d][x_d] = 255
-
-            points = bresenham_line(x_e, y_e, x_d, y_d)
-            for x, y in points:
-                if inverse:
-                    out[y][x] += img[a][i]
-                else:
-                    out[a][i] += img[y][x]
-                # img[y][x] = 100
-
-    out /= out.max()
-    return out
 
 
 @jit
@@ -161,30 +117,31 @@ def convolve_kernel(n=21):
     return kernel
 
 
-if __name__ == "__main__":
-    img = cv2.imread("img/Kropka.jpg", cv2.IMREAD_GRAYSCALE)
-    h, w = img.shape
-    img = cv2.copyMakeBorder(
-        img,
-        int(max((w - h) / 2, 0)),
-        int(max((w - h) / 2, 0)),
-        int(max((h - w) / 2, 0)),
-        int(max((h - w) / 2, 0)),
-        cv2.BORDER_CONSTANT,
-        value=[0],
-    )
-    cv2.imshow("source", img)
-    cv2.waitKey(1)
-
-    sinogram = radon(img, 4, 180)
-    # sinogram = radom_full(img, img.shape, 4, 180, 30)
-    cv2.imshow("sinogram", sinogram)
-    cv2.waitKey(1)
-
-    # Odwrotny Radon
-    # tomograf = radom_full(sinogram, img.shape, 4, 180, 30, inverse=True)
-    tomograf = inverse_radon(sinogram, img.shape, 4, 180)
-
-    cv2.imshow("tomograf", tomograf)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+# if __name__ == "__main__":
+#     import cv2
+#     img = cv2.imread("img/Kropka.jpg", cv2.IMREAD_GRAYSCALE)
+#     h, w = img.shape
+#     img = cv2.copyMakeBorder(
+#         img,
+#         int(max((w - h) / 2, 0)),
+#         int(max((w - h) / 2, 0)),
+#         int(max((h - w) / 2, 0)),
+#         int(max((h - w) / 2, 0)),
+#         cv2.BORDER_CONSTANT,
+#         value=[0],
+#     )
+#     cv2.imshow("source", img)
+#     cv2.waitKey(1)
+#
+#     sinogram = radon(img, 4, 180)
+#     # sinogram = radom_full(img, img.shape, 4, 180, 30)
+#     cv2.imshow("sinogram", sinogram)
+#     cv2.waitKey(1)
+#
+#     # Odwrotny Radon
+#     # tomograf = radom_full(sinogram, img.shape, 4, 180, 30, inverse=True)
+#     tomograf = inverse_radon(sinogram, img.shape, 4, 180)
+#
+#     cv2.imshow("tomograf", tomograf)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
