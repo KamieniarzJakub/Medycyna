2851152e1afabfc7d87e9f288ac8062362cb4b03
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-04-07 21:48:45 +0200

    mapowanie wartosci sinogram

--- src/gui/tomograf_gui.py
+++ src/gui/tomograf_gui.py
@@ -47,50 +47,48 @@ def view_tomograf(
 ):
     st.image(image, "Obraz źródłowy", clamp=True)
 
-    sinogram: npt.NDArray
-    if wyswietl_etapy_posrednie:
-        sinogram = tomograf.radon(
-            image,
-            krok_ukladu,
-            liczba_detektorów,
-            rozwartosc,
-            krok_skanowania,
-            filtrowanie,
-        )
+    if not wyswietl_etapy_posrednie:
+        krok_skanowania = 360
+
+    sinogram = tomograf.radon(
+        image, krok_ukladu, liczba_detektorów, rozwartosc, krok_skanowania, filtrowanie
+    )
+
+    if filtrowanie:
+        col1, col2, col3 = st.columns(3)
+        # col1.image(sinogram, "Sinogram clamped by st", clamp=True)
+        s1 = sinogram.copy()
+        s1[sinogram < 0] = 0
+        col1.image(s1, "Sinogram z odcięciem wartości < 0")
+
+        m = sinogram.min()
+        slope = 1 / (1 - m)
+
+        def f(x):
+            return slope * (x - m)
+
+        s2 = np.vectorize(f)(sinogram.copy())
+        col2.image(s2, "Sinogram po przemapowaniu wartości (odcienie szarości)")
+
+        print(sinogram.shape)
+
+        s3 = np.zeros((sinogram.shape[0], sinogram.shape[1], 3))
+        mask = sinogram >= 0
+        s3[mask, :] = sinogram[mask, np.newaxis]
+        s3[~mask, 0] = np.vectorize(f)(sinogram[~mask])
+        col3.image(s3, "Sinogram po przemapowaniu wartości (kolory)")
     else:
-        sinogram = tomograf.radon(
-            image, krok_ukladu, liczba_detektorów, rozwartosc, run_convolve=filtrowanie
-        )
+        st.image(sinogram, "Sinogram")
 
-    col1, col2 = st.columns(2)
-    col1.image(sinogram, "Sinogram clamped by st", clamp=True)
+    if not wyswietl_etapy_posrednie:
+        krok_odtwarzania = 360
 
-    m = sinogram.min()
-    slope = 1 / (1 - m)
-
-    def f(x):
-        return slope * (x - m)
-
-    s2 = np.vectorize(f)(sinogram.copy())
-    col2.image(s2, "Sinogram remapped by numpy")
-
-    reconstructed: npt.NDArray
-    if wyswietl_etapy_posrednie:
-        reconstructed = tomograf.inverse_radon(
-            sinogram,
-            image.shape,
-            krok_ukladu,
-            liczba_detektorów,
-            rozwartosc,
-            krok_odtwarzania,
-        )
-    else:
-        reconstructed = tomograf.inverse_radon(
-            sinogram,
-            image.shape,
-            krok_ukladu,
-            liczba_detektorów,
-            rozwartosc,
-        )
-
+    reconstructed = tomograf.inverse_radon(
+        sinogram,
+        image.shape,
+        krok_ukladu,
+        liczba_detektorów,
+        rozwartosc,
+        krok_odtwarzania,
+    )
     st.image(reconstructed, "Obraz po odtworzeniu", clamp=True)
