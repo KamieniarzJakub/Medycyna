dd56025048fb93191779ee9a3b7825961ee6a472
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-05-22 14:43:59 +0200

    change code formatting

--- DnoOka/src/main.py
+++ DnoOka/src/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PIL import Image, UnidentifiedImageError
+from PIL import Image, UnidentifiedImageError, ImageFile
 import gzip
 import numpy as np
 from lib import img_processing
@@ -11,13 +11,13 @@ from lib.calc_diff import visualize_array_difference
 
 supported_file_types = ["jpg", "jpeg", "png", "ppm", "gz", "webp", "avif", "gif"]
 
-def read_img(fil: io.BytesIO) -> np.ndarray:
+def read_img(fil: io.BytesIO) -> ImageFile:
     try:
-        return np.asarray(Image.open(fil))
+        return Image.open(fil).copy()
     except UnidentifiedImageError:
         raise Exception("Błędny typ pliku: ", fil.type, ", plik: ", fil.name, ", wspierane rozszerzenia: ", supported_file_types) 
 
-def read_file(file):
+def read_file(file: io.BytesIO) -> ImageFile:
     if file.type == "application/gzip" or file.type == "application/x-gzip":
         with gzip.open(file) as f:
             return read_img(f)
@@ -35,29 +35,32 @@ expected_result = st.file_uploader(
 
 if file is not None:
     image = read_file(file)
-
-    expected_image: np.ndarray
-    if expected_result is not None:
-        expected_image = read_file(expected_result)
+    image_arr = np.asarray(image)
 
     st.image(image, caption="Oryginalny obraz", use_column_width=True, clamp=True)
     tab1, tab2, tab3 = st.tabs(["Wstępne przetwarzanie", "Segmentacja naczyń", "Postprocessing"])
     
+    pre = preprocess_image(image_arr / 255.0)
+    vessels = segment_vessels(pre)
+    final = postprocess_image(vessels)
+
+    expected_image: np.ndarray
+    if expected_result is not None:
+        expected_image_arr = np.asarray(read_file(expected_result).convert("L"))
+        diff = visualize_array_difference(image_arr,expected_image_arr)
+
     with tab1:
-        pre = preprocess_image(image / 255.0)
         st.image(pre, caption="Po wstępnym przetwarzaniu", use_column_width=True, clamp=True)
 
     with tab2:
-        vessels = segment_vessels(pre)
+
         st.image(vessels, caption="Segmentacja naczyń (Frangi)", use_column_width=True, clamp=True)
 
     with tab3:
-        final = postprocess_image(vessels)
         st.image(final, caption="Po końcowym przetwarzaniu", use_column_width=True, clamp=True)
 
         if expected_result is not None:
-            diff = visualize_array_difference(image,expected_image)
             st.image(diff, "Różnica względem obrazu docelowego", clamp=True)
 
-        # mse_result = calc_mse(image / 255, reconstructed)
-        # st.text("Błąd średniokwadratowy: " + str(mse_result))
+            mse_result = (np.asarray(diff)**2).mean()
+            st.text("Błąd średniokwadratowy: " + str(mse_result))
